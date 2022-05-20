be948f6b8afb440fad0995bcc8f9a70d144d380a
dfenghuang <dfenghuang@tencent.com>
2022-05-20 09:29:26 +0800

    update and delete

--- internal/domain/sys/repository/sys_user_repo.go
+++ internal/domain/sys/repository/sys_user_repo.go
@@ -1,8 +1,11 @@
 package repository
 
 import (
+	"errors"
+	"fmt"
 	"go-admin/internal/domain/sys/repository/po"
 	"go-admin/internal/infrastructure/database"
+	"gorm.io/gorm"
 )
 
 type SysUserRepo struct {
@@ -30,4 +33,37 @@ func (r *SysUserRepo) FindById(userId int) (*po.SysUser, error) {
 func (r *SysUserRepo) Save(po *po.SysUser) (int64, error) {
 	result := r.dataSource.DB.Create(po)
 	return result.RowsAffected, result.Error
+}
+
+func (r *SysUserRepo) UpdateById(po *po.SysUser) error {
+	if po.UserId == 0 {
+		return errors.New("Primary must set")
+	}
+	return r.dataSource.DB.Transaction(func(tx *gorm.DB) error {
+		result := tx.Save(po)
+		if result.Error != nil {
+			return result.Error
+		}
+		if result.RowsAffected != 1 {
+			return errors.New(fmt.Sprintf("update failed RowsAffected %d", result.RowsAffected))
+		}
+		return nil
+	})
+}
+
+func (r *SysUserRepo) DeleteById(ids []int) error {
+	count := len(ids)
+	if count == 0 {
+		return errors.New("ids must set")
+	}
+	return r.dataSource.DB.Transaction(func(tx *gorm.DB) error {
+		result := tx.Delete(&po.SysUser{}, ids)
+		if result.Error != nil {
+			return result.Error
+		}
+		if result.RowsAffected != int64(count) {
+			return errors.New(fmt.Sprintf("update failed RowsAffected %d ,Expected %d", result.RowsAffected, count))
+		}
+		return nil
+	})
 }
